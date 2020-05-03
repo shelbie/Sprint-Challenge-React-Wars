34f0141394a22499ce9a4f30f1ce39b813b7798d
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-05-03 18:40:50 -0500

    creating component, axios call with useEffect

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,29 @@
 // Write your Character component here
+import React from "react";
+
+
+function CharacterComponent(props) {
+
+  
+
+  return (
+    <>
+    <div>
+      <h1>{props.name}</h1>
+      <p>{props.height}</p>
+      <p>{props.mass}</p>
+      <p>{props.hairColor}</p>
+      <p>{props.skinColor}</p>
+      <p>{props.eyeColor}</p>
+      <p>{props.gender}</p>
+      <p>{props.homeworld}</p>
+      <p>{props.films}</p>
+      <p>{props.species}</p>
+      <p>{props.vehicles}</p>
+      <p>{props.starships}</p>
+      </div>
+    </>
+  );
+}
+
+export default CharacterComponent;
